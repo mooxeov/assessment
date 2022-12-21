930d9b2302d03e538e762b208313cf1f75cd7e13
jirat.jitbanjerdkul <jirat1995j@gmail.com>
2022-12-21 23:44:17 +0700

    POST Expense

--- expense/expense.go
+++ expense/expense.go
@@ -3,7 +3,41 @@ package expense
 import (
 	"database/sql"
 	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 )
+
+type Expense struct {
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount int      `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
+
+type Err struct {
+	Message string `json:"message"`
+}
+
+func CreateExpenseHandler(c echo.Context) error {
+	var e Expense
+	err := c.Bind(&e)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+	}
+
+	row := db.QueryRow("INSERT INTO expenses (title, amount, note, tags) values($1,$2,$3,$4) RETURNING id,title,amount,note,tags", e.Title, e.Amount, e.Note, pq.Array(e.Tags))
+
+	err = row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, (*pq.StringArray)(&e.Tags))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+	}
+
+	return c.JSON(http.StatusCreated, e)
+}
 
 var db *sql.DB
 
--- serve.go
+++ serve.go
@@ -5,13 +5,19 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/mooxeov/assessment/expense"
 )
 
 func main() {
+	expense.InitDB()
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.POST("/expense", expense.CreateExpenseHandler)
 
 	log.Fatal(e.Start(":2565"))
 }
