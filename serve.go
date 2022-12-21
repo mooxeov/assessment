80d3abfa63f849b0f39ff542a7eb4b24985fd212
jirat.jitbanjerdkul <jirat1995j@gmail.com>
2022-12-21 23:30:49 +0700

    serve run 2565

---
+++ serve.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	log.Fatal(e.Start(":2565"))
+}
